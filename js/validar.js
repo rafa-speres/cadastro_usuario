347b121e25ae60c6ed6aa2f09140de125df4da47
Rafael Scalon Peres <65551999+rafa-speres@users.noreply.github.com>
2024-05-16 23:26:46 -0300

    Update validar.js

--- js/validar.js
+++ js/validar.js
@@ -22,7 +22,7 @@ neste caso o objeto 'nome'
 
 function validarNome(e){ 
     //declaração da expressão regular para definir o formato de um nome válido
-    const regexNome = /^[A-Za-z]+(?: [A-Za-z]+)*$/;
+    const regexNome = /^(?!.*[a-z][A-Z])[A-Z][a-z]*(?: [A-Z][a-z]*)*$/;
     
     console.log(e); //impressão em console do objeto evento e
     console.log(e.target.value); //impressão em console do valor do objeto 'nome' que originou o evento   
